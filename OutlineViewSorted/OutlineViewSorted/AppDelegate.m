3ffa2aa12e43aa3f356c1afe9a5e566a4e435a53
tolik7071 <tolik7071@gmail.com>
2019-09-08 12:50:26 +0300

    Small fix.

--- OutlineViewSorted/OutlineViewSorted/AppDelegate.m
+++ OutlineViewSorted/OutlineViewSorted/AppDelegate.m
@@ -14,14 +14,9 @@
 
 @implementation AppDelegate
 
-- (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
-    // Insert code here to initialize your application
+- (BOOL)applicationShouldTerminateAfterLastWindowClosed:(NSApplication *)sender
+{
+    return YES;
 }
-
-
-- (void)applicationWillTerminate:(NSNotification *)aNotification {
-    // Insert code here to tear down your application
-}
-
 
 @end
